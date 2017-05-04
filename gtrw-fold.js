bdf14e92a2793b236749ebe81ee74cb9dde34918
ghotriw <ghotriw@gmail.com>
2017-05-04 11:13:16 +0300

    Update gtrw-fold.js

--- gtrw-fold.js
+++ gtrw-fold.js
@@ -3,50 +3,47 @@
  * http://ghotriw.pro/
  * Copyright (c) 2017 Andrew Goncharov;
  */
-(function(e){
+(function (e) {
     var methods = {
-        folding : function( options ) {
-            return this.each(function() {
+        folding: function (options) {
+            var selfElement = this,
+                $body = $('body'),
+                defaults = {
+                    block: '.block',
+                    toggle: '.block__toggle',
+                    visibleCssClass: 'block_visible',
+                    bind: 'click',
+                    closeOutClick: true
+                },
+                settings = e.extend(defaults, options);
 
-                var selfElement = this,
-                    $body = $('body'),
-                    defaults = {
-                        block: '.block',
-                        toggle: '.block__toggle',
-                        visibleCssClass: 'block_visible',
-                        bind: 'click',
-                        closeOutClick: true
-                    },
-                    settings = e.extend(defaults, options);
+            $body
+                .on(settings.bind, settings.toggle, function () {
+                    $(this).closest(selfElement).toggleClass(settings.visibleCssClass);
+                });
 
+            if (settings.closeOutClick) {
                 $body
-                    .on(settings.bind, settings.toggle, function () {
-                        $(this).closest(selfElement).toggleClass(settings.visibleCssClass);
+                    .on("click", function (e) {
+                        if (( $(e.target).closest(selfElement).length && $(settings.block).filter('.' + settings.visibleCssClass).length > 0 )
+                            ||
+                            ( $(e.target).closest(selfElement).length )) {
+
+                        } else {
+                            $(this).find('.' + settings.visibleCssClass).removeClass(settings.visibleCssClass);
+                        }
                     });
-
-                if (settings.closeOutClick) {
-                    $body
-                        .on("click", function (e) {
-                            if (( $(e.target).closest(selfElement).length && $(settings.block).filter('.'+settings.visibleCssClass).length > 0 )
-                                ||
-                                ( $(e.target).closest(selfElement).length ))  {
-
-                            } else {
-                                $(this).find('.'+settings.visibleCssClass).removeClass(settings.visibleCssClass);
-                            }
-                        });
-                }
-            })
+            }
         }
     };
 
-    $.fn.gtrwFold = function( method ) {
-        if ( methods[method] ) {
-            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.folding.apply( this, arguments );
+    $.fn.gtrwFold = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.folding.apply(this, arguments);
         } else {
-            $.error( 'Метод с именем ' +  method + ' не существует для jQuery.gtrwFold' );
+            $.error('Метод с именем ' + method + ' не существует для jQuery.gtrwFold');
         }
     };
 })(jQuery);
